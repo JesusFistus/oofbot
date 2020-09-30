e7788d01207062cf198456d37d858ccee8d604eb
Martin <62301536+JesusFistus@users.noreply.github.com>
2020-09-30 19:52:53 +0200

    .

--- src/databasehandler.py
+++ src/databasehandler.py
@@ -1,46 +1,29 @@
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+# SQL-Alchemy initalisierung
+Session = sessionmaker()
+engine = create_engine(f'sqlite:///data/bot.db')
+Base = declarative_base()
+Session.configure(bind=engine)
 
 
-location = 'data/bot.db'
+class CalendarEntry(Base):
+    __tablename__ = 'CalendarEntries'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    category = Column(String)
+    start = Column(Integer)
+    end = Column(Integer)
 
 
-db = sqlite3.connect(location)
-db.row_factory = sqlite3.Row
-cursor = db.cursor()
+class Student(Base):
+    __tablename__ = 'Students'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    discord_id = Column(Integer)
+    name = Column(String)
+    surname = Column(String)
+    study_group = Column(String)
 
-
-def get_entry(table, where):
-    cursor.execute(f'SELECT * from {table} WHERE ?')
-    return dict(cursor.fetchone())
-
-
-def get_table(table):
-    cursor.execute(f'SELECT * FROM {table}')
-    outputlist = []
-    for row in cursor.fetchall():
-        outputlist.append(dict(row))
-    return outputlist
-
-
-def add_entry(table, dict):
-    keystring = ''
-    valuestring = ''
-    for key, value in dict.items():
-        keystring += str(key) + ','
-        if type(value) == str:
-            valuestring += '"' + value + '",'
-        else:
-            valuestring += str(value) + ','
-    keystring = keystring[:-1]
-    valuestring = valuestring[:-1]
-    cursor.execute('INSERT INTO ? WHERE ? VALUES ?', (table, keystring, valuestring))
-    db.commit()
-
-
-def del_entry(table, where):
-    cursor.execute(f'DELETE from {table} WHERE {where}')
-    db.commit()
-
-
-add_entry('Kalender', {'ID': 2, 'Betreff': 'test2', 'Start': 44, 'Ende': 55, 'Wiederholung': 0})
-print(get_table('Kalender'))

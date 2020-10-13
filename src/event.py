591ff30f62ddfc07461068a5b57d3d3c51fb42f5
Martin <62301536+JesusFistus@users.noreply.github.com>
2020-10-13 22:40:23 +0200

    simplified event handling

--- src/event.py
+++ src/event.py
@@ -1,54 +1,21 @@
 import asyncio
 
 eventlist = []
+STOPKEY = '#'
 
 
-class Event:
-    def __init__(self, targetchannel, targetusers=[]):
-        self.inputusers = []
-        self.queue = asyncio.Queue()
-        self.channel = targetchannel
+async def user_input(channel, user):
+    """Waits for a Message from a specifc user in a specific channel.
 
-        if type(targetusers) is list:
-            self.targetusers = targetusers
-
-        else:
-            self.targetusers = [targetusers]
-
-        self.check_current_events()
-        eventlist.append(self)
-
-    async def input(self, targetusers=None):
-        if targetusers is None:
-            self.inputusers = self.targetusers
-
-        elif type(targetusers) == list:
-            self.inputusers = targetusers
-
-        else:
-            self.inputusers = [targetusers]
-
-        message = await self.queue.get()
-
-        if message.content == '#':
-            self.kill()
-            return
-
-        return message
-
-    def kill(self):
-        eventlist.remove(self)
-
-    def check_current_events(self):
-        for event in eventlist:
-            if event.channel == self.channel:  # TODO: Es sollte nur ein Event pro channel UND inputuser geben
-                raise EventError(f'There\'s already an ongoing event in this channel: {str(event.channel)}')
-
-
-async def user_input(targetchannel, targetuser=[]):
-    event = Event(targetchannel, targetuser)
-    output = await event.input()
-    event.kill()
+    Parameters
+    -----------
+    user: [:class:`discord.`]
+        The DiscordClient object which contains all guild-specific information."""
+    event = Event(channel, user)
+    output = await event._input()
+    if output is None:
+        raise EventError(f': {str(event.channel)}')
+    event._kill()
     return output
 
 
@@ -64,3 +31,36 @@ async def check_for_event(message):
 class EventError(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class Event:
+    def __init__(self, channel, user):
+        self.user = user
+        self.channel = channel
+
+        self.queue = asyncio.Queue()
+
+        self._check_current_events()
+        eventlist.append(self)
+
+    async def _input(self):
+        message = await self.queue.get()
+
+        if message.content == STOPKEY:
+            self._kill()
+            raise EventError(f'Event was aborted because the stopkey was received')
+
+        return message
+
+    def _kill(self):
+        try:
+            eventlist.remove(self)
+        except ValueError:
+            print('Could not remove event from eventlist')
+
+    def _check_current_events(self):
+        for event in eventlist:
+            if event.channel == self.channel:
+                raise EventError(f'There\'s already an ongoing event in this channel: {str(event.channel)}')
+            elif event.user == self.user:
+                raise EventError(f'There\'s already an ongoing event for this user: {str(event.user)}')
